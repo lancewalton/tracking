3295f284c9b1fd5fd90ba39e832392faf586f058
Lance Walton <lance.walton@barclays.com>
2014-12-14 14:21:58 +0000

    Tidying up

--- src/main/scala/tracking/Tracking.scala
+++ src/main/scala/tracking/Tracking.scala
@@ -1,24 +1,21 @@
 package tracking
 
-import java.io.{File, FileWriter}
-import scala.xml.{Elem, NodeSeq}
-import scalaz.{Validation, NonEmptyList, Show}
-import tracking.model._
-import tracking.repository.{ProblemLoadingProjectStatusFile, ProjectDirectoryNameParseFailure, RepositoryLoader}
+import java.io.{ File, FileWriter }
+
+import scala.xml.{ Elem, NodeSeq }
+import scala.xml.NodeSeq.seqToNodeSeq
+
+import scalaz.{ NonEmptyList, Show, Validation }
+import scalaz.Validation.FlatMap.ValidationFlatMapRequested
+import scalaz.syntax.show.ToShowOps
+import scalaz.syntax.std.list.ToListOpsFromList
+import scalaz.syntax.std.option.ToOptionOpsFromOption
+
 import org.joda.time.LocalDate
-import scalaz.Validation.FlatMap._
-import scalaz.syntax.std.list._
-import scalaz.syntax.std.option._
-import tracking.model.Complete
-import tracking.model.InProgress
-import tracking.model.NotStarted
-import tracking.model.IdentifierAndTitle
-import tracking.model.Dependency
-import tracking.model.EpicComposition
-  import argonaut._
-  import Argonaut._
-  import tracking.repository._
-  
+
+import tracking.model.{ Complete, DependencyRefersToUnknownEpic, DependencyRefersToUnknownProject, DuplicateEpicId, DuplicateEpicTitle, DuplicateProjectId, DuplicateProjectName, DuplicateReportStatusDate, EmptyEpicId, EmptyEpicTitle, EmptyProjectId, EmptyProjectName, Epic, InProgress, NotStarted, Project, ProjectStatus, Repository }
+import tracking.repository.{ ProblemLoadingProjectStatusFile, ProjectDirectoryNameParseFailure, RepositoryLoader }
+
 object Tracking extends App {
   val repository: Validation[NonEmptyList[Object], Repository] =
     for {
@@ -26,7 +23,7 @@ object Tracking extends App {
       repository <- Repository.createValid(projects)
     } yield repository
 
-  repository bimap(reportErrors, generateReport) foreach saveReport
+  repository bimap (reportErrors, generateReport) foreach saveReport
 
   private def reportErrors(errors: NonEmptyList[_]): Unit = {
     import scalaz.syntax.show._
@@ -44,14 +41,14 @@ object Tracking extends App {
       case DuplicateProjectId(id) => s"Duplicate project id: '$id'"
       case EmptyProjectId => "Empty project id"
       case DuplicateReportStatusDate(project, date) => s"Duplicate report status date in project ${project.identifiers.show}: ${date.show}"
-        case EmptyEpicTitle(project, status, epic) => s"Empty epic title in project ${project.identifiers.show}, status ${status.date.show}: ${epic.identifiers.show}"
+      case EmptyEpicTitle(project, status, epic) => s"Empty epic title in project ${project.identifiers.show}, status ${status.date.show}: ${epic.identifiers.show}"
       case DuplicateEpicTitle(project, status, title) => s"Duplicate epic title in project ${project.identifiers.show}, status ${status.date.show}: '$title'"
-        case EmptyEpicId(project, status, epic) => s"Empty epic id in project ${project.identifiers.show}, status ${status.date.show}: ${epic.identifiers.show}"
+      case EmptyEpicId(project, status, epic) => s"Empty epic id in project ${project.identifiers.show}, status ${status.date.show}: ${epic.identifiers.show}"
       case DuplicateEpicId(project, status, id) => s"Duplicate epic title in project ${project.identifiers.show}, status ${status.date.show}: '$id'"
       case DependencyRefersToUnknownProject(project, status, dependency) => s"Dependency ${dependency.show} in status ${status.date.show} of project ${project.identifiers.show} refers to non-existent project id '${dependency.projectId}'"
       case DependencyRefersToUnknownEpic(project, status, dependency) => s"Dependency ${dependency.show} in status ${status.date.show} of project ${project.identifiers.show} refers to non-existent epic id '${dependency.epicId}'. The referenced project was found, but it does not contain the referenced epic."
       case e => s"Unknown error: $e"
-    }.foreach { msg => println(s"\t$msg")}
+    }.foreach { msg => println(s"\t$msg") }
   }
 
   private def saveReport(report: NodeSeq): Unit = {
@@ -70,7 +67,7 @@ object Tracking extends App {
         <script type="text/javascript" src="libs/amcharts/themes/chalk.js"></script>
       </head>
       <body>
-        <h1>BOOST Status</h1>{renderProjects(repository)}
+        <h1>BOOST Status</h1>{ renderProjects(repository) }
       </body>
     </html>
   }
@@ -80,7 +77,7 @@ object Tracking extends App {
 
   private def renderProject(repository: Repository, project: Project): Elem =
     <div>
-      {renderProjectBody(repository, project)}
+      { renderProjectBody(repository, project) }
     </div>
 
   private def renderProjectBody(repository: Repository, project: Project): NodeSeq =
@@ -90,7 +87,7 @@ object Tracking extends App {
     project.statuses.toNel.cata(renderReportBody, <h2>No data</h2>)
 
   private def renderReportBody(statuses: NonEmptyList[ProjectStatus]): NodeSeq = {
-    val sortedStatuses = statuses.sorted
+    val sortedStatuses = statuses.sorted.reverse
     renderStatus(sortedStatuses.head) ++ Burndown(sortedStatuses)
   }
 
@@ -98,22 +95,22 @@ object Tracking extends App {
     renderEpicProgressBar(status) ++
       <div class="all-epics">
         <div class="completed-epics-list">
-          {renderCompletedEpics(status)}
+          { renderCompletedEpics(status) }
         </div>
         <div class="incomplete-epics-list">
-          {renderIncompleteEpics(status)}
+          { renderIncompleteEpics(status) }
         </div>
         <div class="unstarted-epics-list">
-          {renderNotStartedEpics(status)}
+          { renderNotStartedEpics(status) }
         </div>
       </div>
 
   private def renderTitle(title: String): Elem = <h2>
-    {title}
-  </h2>
+                                                   { title }
+                                                 </h2>
 
   private def renderEpicProgressBar(status: ProjectStatus) =
-    renderProgressBar(status.epicsWithStatus(Complete).size,status.epicsWithStatus(InProgress).size, status.epicsWithStatus(NotStarted).size)
+    renderProgressBar(status.epicsWithStatus(Complete).size, status.epicsWithStatus(InProgress).size, status.epicsWithStatus(NotStarted).size)
 
   private def renderProgressBar(completed: Int, inProgress: Int, notStarted: Int) = {
     val total = completed + inProgress + notStarted
@@ -122,13 +119,13 @@ object Tracking extends App {
     val unstartedPercentage = 100 - completedPercentage - inProgressPercentage
 
     <div class="progress">
-      <div class="progress-bar progress-bar-success" style={s"width: $completedPercentage%"}>
+      <div class="progress-bar progress-bar-success" style={ s"width: $completedPercentage%" }>
         <span>Completed</span>
       </div>
-      <div class="progress-bar progress-bar-warning progress-bar" style={s"width: $inProgressPercentage%}"}>
+      <div class="progress-bar progress-bar-warning progress-bar" style={ s"width: $inProgressPercentage%}" }>
         <span>In Progress</span>
       </div>
-      <div class="progress-bar progress-bar-danger" style={s"width: $unstartedPercentage%"}>
+      <div class="progress-bar progress-bar-danger" style={ s"width: $unstartedPercentage%" }>
         <span>Not Started</span>
       </div>
     </div>
@@ -139,11 +136,12 @@ object Tracking extends App {
 
   private def epicTable(name: String, epics: NonEmptyList[Epic]): NodeSeq =
     <h3>
-      {name}
+      { name }
     </h3> ++ {
-      epics.list.map { epic => <div>
-        {epic.identifiers.title}
-      </div>
+      epics.list.map { epic =>
+        <div>
+          { epic.identifiers.title }
+        </div>
       }
     }
 
@@ -159,7 +157,7 @@ object Tracking extends App {
       status.map { epic =>
         <div class="incomplete-epic-row">
           <div class="in-progress-epic-name">
-            {epic.identifiers.title}
+            { epic.identifiers.title }
           </div>
           <div class="in-progress-epic-completion">
             { renderProgressBar(epic.composition.fold(0)(_.completedStories), epic.composition.fold(1)(_.storiesInProgress), epic.composition.fold(0)(_.unstartedStories)) }
